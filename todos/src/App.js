f8be0ea9a44a4f6d5d0c0ec7eaca686f5e8ceef4
kimtaeung020322 <122251763+kimtaeung020322@users.noreply.github.com>
2023-01-27 16:28:48 +0900

    Update App.js

--- todos/src/App.js
+++ todos/src/App.js
@@ -1,6 +1,6 @@
-import "./index.css"; //webpack이 img, css 같은 정적 파일을 로딩할 수 있게 해준다.
 import Container from "./component/Container";
 import { ThemeProvider } from "styled-components";
+import "./index.css"; // webpack이 img, css 같은 정적 파일을 로딩할 수 있게 해준다.
 import Button from "./component/Button";
 
 function App() {
@@ -8,12 +8,12 @@ function App() {
     <ThemeProvider
       theme={{
         colors: {
-          main_color: "#4da24",
-          active_color: "#4da24",
+          main_color: "#74da24",
+          active_color: "#de4381",
         },
       }}
     >
-      <Button text="Login" bgColor="red" big="true" />
+      <Button text="Login" bgColor="red" big />
       <Button text="Sign Up" />
     </ThemeProvider>
   );
