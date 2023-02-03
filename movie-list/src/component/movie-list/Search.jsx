2076905db302fdf7691297f9831fa00af4a54a86
kimtaeung020322 <03n22@naver.com>
2023-02-03 17:59:25 +0900

    20230203_KTU

--- movie-list/src/component/movie-list/Search.jsx
+++ movie-list/src/component/movie-list/Search.jsx
@@ -55,7 +55,7 @@ function Search() {
       />
       <ResultList>
         {searchResult?.map((data) => (
-          <SearchResultItem data={data} />
+          <SearchResultItem key={data.id} data={data} />
         ))}
       </ResultList>
     </Container>
